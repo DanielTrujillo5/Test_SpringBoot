2aad55bad1c3014f68a35d0ffcf475a869e7e1a3
DanielTrujillo5 <danieltigo555@gmail.com>
2026-02-18 09:00:00 -0500

    Subiendo correctamente MyFisrtDBPry

---
+++ MyFisrtDBPry/src/test/java/com/example/MyFisrtDBPry/MyFisrtDbPryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MyFisrtDBPry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyFisrtDbPryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
